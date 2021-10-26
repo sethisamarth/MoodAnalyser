d7afe03048f4cfe685631b94b9b3226ded9e46f3
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-27 00:47:14 +0530

    Mood Analyse happy or sad

---
+++ MoodAnalyser/MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserProb
+{
+   public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
